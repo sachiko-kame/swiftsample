793b781c2f6af122d254012fd77102ce87f56e31
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-06-17 22:41:32 +0900

    frameworkの追加
    今回ドッロップ追加だがAdd toでもいいらしい。

---
+++ objframework.framework/Headers/objframework.h
@@ -0,0 +1,20 @@
+//
+//  objframework.h
+//  objframework
+//
+//  Created by 水野祥子 on 2017/06/17.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "sample1.h"
+
+//! Project version number for objframework.
+FOUNDATION_EXPORT double objframeworkVersionNumber;
+
+//! Project version string for objframework.
+FOUNDATION_EXPORT const unsigned char objframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objframework/PublicHeader.h>
+
+
---
+++ objframework.framework/Headers/sample1.h
@@ -0,0 +1,13 @@
+//
+//  sample1.h
+//  objframework
+//
+//  Created by 水野祥子 on 2017/06/17.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sample1 : NSObject
+-(void)sampletext;
+@end
